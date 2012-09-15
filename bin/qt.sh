bfe92e113ba0233d9a6992bc75520f8e00335d35
Denise Case <dmcase@ksu.edu>
2012-09-15 07:15:19 -0500

    more scripts

--- bin/qt.sh
+++ bin/qt.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
-# Description: qt.sh is a quick test to make sure nothing is wrong 
-# Notes: To run this script, type . qt.sh in the terminal 
-cd ~/CIS520/Pintos/pintos/src/threads
+# qt.sh = a "Quick Test" to be sure nothing is broken; just runs alarm-multiple
+#
+# To run this script, type ". qt.sh" in the putty terminal window.
+# If it doesn't work, this file may not be in your path or your folder
+# capitalization is different (e.g. "Pintos" instead of "pintos" in the first line).
+#
+# Recommendation:
+#   Add ~/cis520/Pintos/bin to your path 
+#
+cd ~/cis520/Pintos/pintos/src/threads
 make clean
 make
 cd build
